6ad3feba3c7e3b1789e12da3d9957e1e49358c7d
oosawa_k <freedom.red.green@gmail.com>
2022-05-23 18:56:06 +0900

    グローバルナビゲーション追加

--- functions.php
+++ functions.php
@@ -2,3 +2,4 @@
     locate_template('functions/eyecatch.php', true);    //アイキャッチ画像の読み込み
     locate_template('functions/customPost.php',true );  //カスタム投稿設定
     locate_template('functions/gutenberg.php', true);   //グーテンベルク設定
+    locate_template('functions/globalNavigation.php',true); //グローバルナビ設定
