38148160b1c65be0ec183670c1791d2ae47c4e0a
oosawa_k <freedom.red.green@gmail.com>
2022-06-11 10:17:55 +0900

    prof用css追加

--- parts/page-prof/header.php
+++ parts/page-prof/header.php
@@ -18,7 +18,7 @@
                 h = d.documentElement, t = setTimeout(function () { h.className = h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive"; }, config.scriptTimeout), tk = d.createElement("script"), f = false, s = d.getElementsByTagName("script")[0], a; h.className += " wf-loading"; tk.src = 'https://use.typekit.net/' + config.kitId + '.js'; tk.async = true; tk.onload = tk.onreadystatechange = function () { a = this.readyState; if (f || a && a != "complete" && a != "loaded") return; f = true; clearTimeout(t); try { Typekit.load(config) } catch (e) { } }; s.parentNode.insertBefore(tk, s)
         })(document);
     </script>
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/front-page/ress.css">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/front-page/index_sp.css" media="screen and (max-width:590px)">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/front-page/index.css" media="screen and (min-width:591px)">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/page-prof/ress.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/page-prof/index_sp.css" media="screen and (max-width:590px)">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/page-prof/index.css" media="screen and (min-width:591px)">
     <?php wp_head(); ?>
