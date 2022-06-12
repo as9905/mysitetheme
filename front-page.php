07cfb7530f0af153962bb5aee89a00989f8d8b1b
oosawa_k <freedom.red.green@gmail.com>
2022-06-12 17:18:19 +0900

    front-pageレンダリング修正

--- front-page.php
+++ front-page.php
@@ -11,7 +11,7 @@
     </noscript>
     <div class="body-wrapper">
         <?php get_template_part('parts/front-page/header-nav'); ?>
-        <div class="post-list">
+        <div class="blog">
             <?php
             $args = array(
                 'posts_per_page' => 5 // 表示件数の指定
@@ -19,15 +19,15 @@
             if (have_posts()) :
             ?>
                 <?php while (have_posts()) : the_post(); ?>
-                    <article>
-                        <h2 class="post-title"><?php the_title(); ?></h2><!-- /.post-title -->
+                    <article class="blog-inner">
+                        <h2 class="blog-post__title"><?php the_title(); ?></h2><!-- /.post-title -->
                         <p><?php the_time(get_option('date_format')); ?></p>
                     </article>
                 <?php endwhile; ?>
             <?php else : ?>
                 <p>記事がありません</p>
             <?php endif; ?>
-        </div><!-- /.post-list -->
+        </div><!-- /.blog -->
         <?php get_template_part('parts/front-page/footer-nav'); ?>
     </div> <!-- /.body-wrapper -->
     <?php get_template_part('parts/front-page/footer'); ?>
