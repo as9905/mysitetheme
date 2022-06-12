3776c12d21f94d69bad9eca9d7781439e3748c5e
oosawa_k <freedom.red.green@gmail.com>
2022-06-12 18:39:26 +0900

    PC版ブログタイトルレイアウト修正

--- front-page.php
+++ front-page.php
@@ -12,22 +12,24 @@
     <div class="body-wrapper">
         <?php get_template_part('parts/front-page/header-nav'); ?>
         <div class="blog">
-            <?php
-            $args = array(
-                'posts_per_page' => 5 // 表示件数の指定
-            );
-            if (have_posts()) :
-            ?>
-                <?php while (have_posts()) : the_post(); ?>
-                    <article class="blog-inner">
-                        <h2 class="blog-post__title"><?php the_title(); ?></h2><!-- /.post-title -->
-                        <p><?php the_time(get_option('date_format')); ?></p>
-                    </article>
-                <?php endwhile; ?>
-            <?php else : ?>
-                <p>記事がありません</p>
-            <?php endif; ?>
-        </div><!-- /.blog -->
+            <div class="blog-inner">
+                <?php
+                $args = array(
+                    'posts_per_page' => 5 // 表示件数の指定
+                );
+                if (have_posts()) :
+                ?>
+                    <?php while (have_posts()) : the_post(); ?>
+                        <article class="blog-post">
+                            <h2 class="blog-post__title"><?php the_title(); ?></h2><!-- /.post-title -->
+                            <p class="blog-post__date"><?php the_time(get_option('date_format')); ?></p>
+                        </article>
+                    <?php endwhile; ?>
+                <?php else : ?>
+                    <p>記事がありません</p>
+                <?php endif; ?>
+            </div><!-- /.blog-inner -->
+            </div><!-- /.blog -->
         <?php get_template_part('parts/front-page/footer-nav'); ?>
     </div> <!-- /.body-wrapper -->
     <?php get_template_part('parts/front-page/footer'); ?>
