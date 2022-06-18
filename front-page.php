d804cba7ac3aa8a18677094dfffa586cff8ff6db
as9905 <freedom.red.green@gmail.com>
2022-06-18 15:32:50 +0900

    front-page.php記事リンク修正中

--- front-page.php
+++ front-page.php
@@ -19,14 +19,13 @@
                 ?>
                     <?php while (have_posts()) : the_post(); ?>
                         <article class="blog-post">
-                            <a href="<?php the_permalink(); ?>">
-                                <div class="blog-post__heading">
-                                    <h2 class="blog-post__title"><?php the_title(); ?></h2><!-- /.post-title -->
-                                    <p class="blog-post__date"><?php the_time(get_option('date_format')); ?></p>
-                                </div>
+                            <div class="blog-post__heading">
+                                <h2 class="blog-post__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2><!-- /.post-title -->
+                                <p class="blog-post__date"><?php the_time(get_option('date_format')); ?></p>
+                            </div>
                                 <p class="blog-post__excerpt"><?php echo get_the_excerpt(); ?></p>
-                            </a><!-- /. -->
-                        </article>
+                            
+                        </article><!-- /.blog-post -->
                     <?php endwhile; ?>
                 <?php else : ?>
                     <p>記事がありません</p>
