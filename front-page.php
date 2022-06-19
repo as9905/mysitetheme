96862ec5b5c85748829245dbc2bfbe44738800f3
as9905 <freedom.red.green@gmail.com>
2022-06-20 00:25:44 +0900

    ページネーション修正中

--- front-page.php
+++ front-page.php
@@ -6,8 +6,8 @@
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-        <div class="body-wrapper">
+    <?php wp_body_open(); ?>
+    <div class="body-wrapper">
         <?php get_template_part('parts/front-page/header-nav'); ?>
         <div class="blog">
             <div class="blog-inner">
@@ -15,41 +15,42 @@
                 $args = array(
                     'posts_per_page' => 5 // 表示件数の指定
                 );
-                if (have_posts()) :
+                $posts = get_posts($args);
+                foreach ($posts as $post) : // ループの開始
+                    setup_postdata($post); // 記事データの取得
                 ?>
-                    <?php while (have_posts()) : the_post(); ?>
-                        <article class="blog-post">
-                            <a href="<?php the_permalink(); ?>" class="blog-post__link"></a>
-                            <div class="blog-post__heading">
-                                <h2 class="blog-post__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2><!-- /.post-title -->
-                                <p class="blog-post__date"><?php the_time(get_option('date_format')); ?></p>
-                            </div>
-                                <p class="blog-post__excerpt"><?php echo get_the_excerpt(); ?></p>
-                        </article><!-- /.blog-post -->
-                    <?php endwhile; ?>
-                <?php else : ?>
-                    <p>記事がありません</p>
-                <?php endif; ?>
+                    <article class="blog-post">
+                        <a href="<?php the_permalink(); ?>" class="blog-post__link"></a>
+                        <div class="blog-post__heading">
+                            <h2 class="blog-post__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2><!-- /.post-title -->
+                            <p class="blog-post__date"><?php the_time(get_option('date_format')); ?></p>
+                        </div>
+                        <p class="blog-post__excerpt"><?php echo get_the_excerpt(); ?></p>
+                    </article><!-- /.blog-post -->
+                <?php
+                    endforeach; // ループの終了
+                    wp_reset_postdata(); // 直前のクエリを復元する
+                ?>
             </div><!-- /.blog-inner -->
             <div class="blog-pager">
                 <?php
-                    $link = get_previous_posts_link('≪　新しい記事へ');
-                    if($link){
-                        $link = str_replace('<a', '<a class="blog-pager__btn pager-btn__left"', $link);
-                        $link = str_replace('/a>', '/a><!-- /.blog-pager__btn /.pager-btn__left -->', $link);
-                        echo $link;
-                    }
+                $link = get_previous_posts_link('≪　新しい記事へ');
+                if ($link) {
+                    $link = str_replace('<a', '<a class="blog-pager__btn pager-btn__left"', $link);
+                    $link = str_replace('/a>', '/a><!-- /.blog-pager__btn /.pager-btn__left -->', $link);
+                    echo $link;
+                }
                 ?>
                 <?php
-                    $link = get_next_posts_link('古い記事へ　≫');
-                    if($link){
-                        $link = str_replace('<a', '<a class="blog-pager__btn pager-btn__right"', $link);
-                        $link = str_replace('/a>', '/a><!-- /.blog-pager__btn /.pager-btn__right -->', $link);
-                        echo $link;
-                    }
+                $link = get_next_posts_link('古い記事へ　≫');
+                if ($link) {
+                    $link = str_replace('<a', '<a class="blog-pager__btn pager-btn__right"', $link);
+                    $link = str_replace('/a>', '/a><!-- /.blog-pager__btn /.pager-btn__right -->', $link);
+                    echo $link;
+                }
                 ?>
             </div><!-- /.blog-pager -->
-        </div><!-- /.blog -->        
+        </div><!-- /.blog -->
         <?php get_template_part('parts/front-page/footer-nav'); ?>
     </div> <!-- /.body-wrapper -->
     <?php get_template_part('parts/front-page/footer'); ?>
